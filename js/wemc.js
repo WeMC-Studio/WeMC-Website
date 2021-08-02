b2956b310710d23315a13df293185730f07eb5fd
WYH2004 <32890512+VIPWYH2004@users.noreply.github.com>
2021-08-02 16:17:12 +0800

    2021-08-02-16-16

--- js/wemc.js
+++ js/wemc.js
@@ -1,3 +1,12 @@
 $(function(){
     $('#fullpage').fullpage();
 });
+
+$(function(){
+    if($(window).height()==$(document).height()){
+      $(".footer").addClass("navbar-fixed-bottom");
+    }
+    else{
+      $(".footer").removeClass(" navbar-fixed-bottom");
+    }
+  });
