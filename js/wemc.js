de59f40478d571c8ddfcd2def0f4feaf62e6961f
WYH2004 <32890512+VIPWYH2004@users.noreply.github.com>
2021-08-02 23:45:11 +0800

    2021-08-02-23-45

--- js/wemc.js
+++ js/wemc.js
@@ -10,3 +10,25 @@ $(function(){
       $(".footer").removeClass(" navbar-fixed-bottom");
     }
   });
+
+$(function(){
+    var bodyBgs = [];
+	bodyBgs[0] = "长风破浪会有时，直挂云帆济沧海。";
+	bodyBgs[1] = "人生自古谁无死？留取丹心照汗青。";
+	bodyBgs[2] = "宝剑锋从磨砺出，梅花香自苦寒来。";
+	bodyBgs[3] = "回首向来萧瑟处，归去，也无风雨也无晴。";
+	bodyBgs[4] = "石可破也，而不可夺坚；丹可磨也，而不可夺赤。";
+	bodyBgs[5] = "众里寻他千百度，蓦然回首，那人却在，灯火阑珊处。";
+	bodyBgs[6] = "http://file.wyh2004.top/background/07.jpg";
+	bodyBgs[7] = "http://file.wyh2004.top/background/08.png";
+	bodyBgs[8] = "http://file.wyh2004.top/background/09.jpg";
+	bodyBgs[9] = "http://file.wyh2004.top/background/10.jpg";
+	bodyBgs[10] = "http://file.wyh2004.top/background/11.jpg";
+	bodyBgs[11] = "http://file.wyh2004.top/background/12.jpg";
+	bodyBgs[12] = "http://file.wyh2004.top/background/13.png";
+ 
+	var randomBgIndex = Math.round( Math.random() * 5 );
+ 
+    document.getElementById("mdjl-text").innerHTML=bodyBgs[randomBgIndex];
+	
+});
